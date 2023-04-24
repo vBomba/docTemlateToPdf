7b3701e1457eeee9719ed2c94c25bf84e3aed065
Vasyl Bomba <bombavasyl@gmial.com>
2023-04-24 21:39:30 +0200

    use libreoffice convert

--- script.js
+++ script.js
@@ -1,56 +1,47 @@
 const AdmZip = require("adm-zip");
+const libre = require("libreoffice-convert");
 const path = require("path");
 const fs = require("fs");
-const { promisify } = require("util");
-const exec = promisify(require("child_process").exec);
 
-async function replaceTextAndSaveToPdf(
-  entryObj,
-  entryFileName,
-  outputDocxName,
-  outputPdfName
-) {
-  // Load the file as a zip archive
+function replaceTextAndSaveToPdf(entryObj, entryFileName, outputPdfName) {
   const zip = new AdmZip(entryFileName);
-
-  // Read the contents of the "word/document.xml" file
   const xmlContent = zip.readAsText("word/document.xml");
-
   const regEx = `(${Object.keys(entryObj)
     .map((v) => `{${v}}`)
     .join("|")})`;
-
-  // Replace the text
   const modifiedContent = xmlContent.replace(
     new RegExp(regEx, "g"),
     function (c) {
       for (const [key, value] of Object.entries(entryObj)) {
         if (c === `{${key}}`) {
-          return value;
+          if (typeof value === "string") {
+            return value;
+          }
         }
       }
     }
   );
 
-  // Write the modified content back to the zip archive
-  zip.updateFile("word/document.xml", Buffer.from(modifiedContent));
-
-  // Save the modified zip archive to a new file
-  zip.writeZip(outputDocxName);
-
-  const outputPath = path.join(__dirname, outputPdfName);
-
-  // Use LibreOffice to convert the docx file to pdf
-  const { stderr } = await exec(
-    `soffice --convert-to pdf --outdir "${__dirname}" "${outputDocxName}"`
+  const tmpDocxPath = path.join(
+    __dirname,
+    `${Math.random().toString(36).substring(2, 15)}.docx`
   );
-  if (stderr) {
-    console.error(`Error converting file: ${stderr}`);
-    return;
-  }
-
-  // Rename the output pdf file
-  fs.renameSync(path.join(__dirname, `${outputDocxName}.pdf`), outputPath);
+  zip.updateFile("word/document.xml", Buffer.from(modifiedContent));
+  zip.writeZip(tmpDocxPath);
+  const outputPath = path.join(__dirname, outputPdfName);
+  const docxPdf = path.join(
+    __dirname,
+    `${Math.random().toString(36).substring(2, 15)}.pdf`
+  );
+  const enterPath = fs.readFileSync(tmpDocxPath);
+  libre.convert(enterPath, ".pdf", undefined, (err, done) => {
+    if (err) {
+      console.log(`Error converting file: ${err}`);
+    }
+    fs.writeFileSync(docxPdf, done);
+    fs.renameSync(docxPdf, outputPath);
+    fs.unlinkSync(tmpDocxPath);
+  });
 }
 
 replaceTextAndSaveToPdf(
@@ -59,6 +50,5 @@ replaceTextAndSaveToPdf(
     y: "test2",
   },
   "document.docx",
-  "docx_mod.docx",
   "pdf_mod.pdf"
 );
